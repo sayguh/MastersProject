716182633c5906c0abc11be001177705021ffd59
Youssef Bagoulla <ylb@pico.(none)>
2013-02-11 21:47:15 -0500

    Uploading the scanner I intended to modify and use as my energy detector

---
+++ gnublocks/gr_scan/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lgnuradio-core -ldl -lpthread -lgnuradio-osmosdr
+
